08ae48aaf618c8cc720803962137c86924e69688
Adam Narkunski <airdong@gmail.com>
2016-11-23 19:03:18 +0200

    Stay on the last branch when loop is done

--- take_me_to_develop.py
+++ take_me_to_develop.py
@@ -3,26 +3,6 @@ import logging
 from git import Repo, Git
 import re
 
-"""
-Take Me To Develop!
-ABOUT:
-This tool detects a release branch you're on (of the form release-M.mm.0),
-and merges it to all future release branches and then to develop.
-for example, if you're on release-2.52.0, and you run the tool, it should perform these merges:
-
-    1. merge release-2.52.0 -> release-2.53.0
-    2. push release-2.53.0
-    3. release-2.53.0 -> develop
-    4. push develop
-
-In case of a conflict or other git errors, it will fail spectacularly.
-
-USAGE:
-Just run it inside the repo, with a release branch you want to merge checked out.
-
-Author: Adam Narkunski
-"""
-
 FORMAT = '%(message)s'
 logging.basicConfig(format=FORMAT)
 logger = logging.getLogger('take')
@@ -66,8 +46,6 @@ class TakeMe(object):
             # 5. go to the next release branch, or develop
             last_branch = self.get_current_branch()
 
-        logger.info('checking out original branch')
-        self.initial_branch.checkout()
 
     def get_current_branch(self):
         logger.debug("current branch is '%s'", self.repo.active_branch)
@@ -114,10 +92,8 @@ def test(tm):
 
 def main():
     tm = TakeMe("/Users/adam/repos/test/")
-    test(tm)
+    # test(tm)
     tm.main()
-
-
 
 if __name__ == "__main__":
     main()
